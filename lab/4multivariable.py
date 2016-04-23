b10e5f8621fd8a030de981118c0828e9d8bb14cc
bart.lee <bart.lee@daumkakao.com>
2016-04-23 15:03:06 +0900

    include b

--- lab/4multivariable.py
+++ lab/4multivariable.py
@@ -1,18 +1,17 @@
 import tensorflow as tf
 
-x1_data = [1, 0, 3, 0, 5]
-x2_data = [0, 2, 0, 4, 0]
+x_data = [[1, 1, 1, 1, 1],
+          [1., 0., 3., 0., 5],
+          [0., 2., 0., 4., 0]]
+
 y_data = [1, 2, 3, 4, 5]
 
-W1 = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
-W2 = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
-
-b = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
+W = tf.Variable(tf.random_uniform([1, 3], -1.0, 1.0))
 
 # X = tf.placeholder(tf.float32)
 # Y = tf.placeholder(tf.float32)
 
-hypothesis = W1 * x1_data + W2 * x2_data + b
+hypothesis = tf.matmul(W, x_data)
 
 cost = tf.reduce_mean(tf.square(hypothesis - y_data))
 
@@ -28,4 +27,4 @@ sess.run(init)
 for step in xrange(2001):
     sess.run(train)
     if step % 20 == 0:
-        print step, sess.run(cost), sess.run(W1), sess.run(W2), sess.run(b)
+        print step, sess.run(cost), sess.run(W)
